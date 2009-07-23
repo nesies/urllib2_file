6f41fdd0ba8f1472363a9e38d3e10ab4e74d8cd0
Fabien <seisen@gmail.com>
2009-07-23 17:23:07 +0200

    - Fix name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(name="urllib2_file",
-	version='0.2.0',
-	description='urllib2 extension which permit uploading file',
+	version='0.2.1',
+	description='urllib2 extension which permit HTTP POST upload',
 	author='Fabien SEISEN',
 	author_email='fab@seisen.org',
 	url='http://fabien.seisen.org/python/',
